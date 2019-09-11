e3e80a906ec4143a77d2b6e905eb584c19c0bdce
Omulosi <mulongojohnpaul@gmail.com>
2019-09-11 15:57:46 +0300

    Create componenst. No return UI description yet

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Header = () => {
+
+  return (
+    <div className="header">
+      <h1>NASA photo of the day</h1>
+    </div>
+  )
+}
+
+export default Header;
