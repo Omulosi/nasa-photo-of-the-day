e39d57c09587990a33bf7bb74f409e5878e48f7e
Omulosi <mulongojohnpaul@gmail.com>
2019-09-11 15:56:22 +0300

    Add Date component and styles. Add dummy data to components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
+import img from "./assets/mountains.jpeg";
 
 import Header from './components/Header';
 import Date from './components/Date';
@@ -19,9 +20,9 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Date date={date}/>
-      <Image title={title} imgSrc={url} explanation={explanation} />
-      <Footer copyright={copyright} />
+      <Date date="2019-09-11"/>
+      <Image title="IC 1805: The Heart Nebula" imgSrc={img} explanation="What energizes the Heart Nebula? First, the large emission nebula dubbed IC 1805 looks, in whole, like a human heart.  The nebula glows brightly in red light emitted by its most prominent element: hydrogen.  The red glow and the larger shape are all powered by a small group of stars near the nebula's center." />
+      <Footer copyright="Bray Falls" />
     </div>
   );
 }
