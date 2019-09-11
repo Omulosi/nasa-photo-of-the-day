40aef7fea8bddc1c5b405d1fab75a3e86eed6493
Omulosi <mulongojohnpaul@gmail.com>
2019-09-11 16:19:29 +0300

    Create Image component using dummy data

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,8 +4,19 @@ import React from 'react';
 
 const Image = (props) => {
 
+  const {title, imgSrc, explanation} = props;
+
   return (
-    <div></div>
+    <div className="main-content">
+      <figure className="main-photo">
+        <img src={imgSrc} alt="photo of the day"/>
+        <figcaption>{title}</figcaption>
+      </figure>
+
+      <div className="explanation">
+        <p>{explanation}</p>
+      </div>
+    </div>
   );
 }
 
