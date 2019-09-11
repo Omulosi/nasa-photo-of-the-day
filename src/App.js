d693afff547a48e8a60bade045f2aed03e19ed5a
Omulosi <mulongojohnpaul@gmail.com>
2019-09-11 15:05:18 +0300

    Initial set up
    Set up overall structure of the app and define the components it is going
    to have, plus the prop values they will accept.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+import Header from './components';
+import Date from './components';
+import Image from './components';
+import Footer from './components';
+
 function App() {
+  const [state, setState] = useState({
+    date:'', 
+    explanation:'',
+    url: '',
+    title: '',
+    copyright: ''
+  })
+
+  const {date, title, copyright, url, explanation} = state;
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <Header />;
+      <Date date={date}/>
+      <Image title={title} imgSrc={url} explanation={explanation} />
+      <Footer copyright={copyright} />
     </div>
   );
 }
